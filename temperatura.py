963faff586a529e7188c0d29a06aff55cd7018c5
LAIAAAAAAAAAAAAAAAAAAAAAAAAA <laia.bru@iaballester.cat>
2026-05-15 13:29:31 +0200

    Actualització automàtica

--- temperatura.py
+++ temperatura.py
@@ -2,8 +2,10 @@ import random
 import datetime
 import csv
 import os
+import json
 
-FILE = "temperatures.csv"
+FILE_CSV = "temperatures.csv"
+FILE_JSON = "temperatures.json"
 
 def generar_temperatura():
     return round(random.uniform(10, 30), 2)
@@ -11,17 +13,25 @@ def generar_temperatura():
 def guardar_dada():
     data = datetime.datetime.now()
     temperatura = generar_temperatura()
+    existeix = os.path.isfile(FILE_CSV)
 
-    existeix = os.path.isfile(FILE)
-
-    with open(FILE, mode='a', newline='') as f:
+    with open(FILE_CSV, mode='a', newline='') as f:
         writer = csv.writer(f)
-
         if not existeix:
             writer.writerow(["data", "temperatura"])
-
         writer.writerow([data.strftime("%Y-%m-%d %H:%M:%S"), temperatura])
 
     print(f"Dada guardada: {data} -> {temperatura}°C")
+
+    dades = []
+    with open(FILE_CSV, newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            dades.append({"data": row["data"], "temperatura": float(row["temperatura"])})
+
+    with open(FILE_JSON, 'w') as f:
+        json.dump(dades, f, indent=2)
+
+    print("JSON actualitzat.")
 
 guardar_dada()
